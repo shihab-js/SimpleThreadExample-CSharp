d83e29e6f1dc5e21e7524aa23a0c6121246cb96b
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-29 22:52:43 +0600

    Add project files.

---
+++ SimpleThreadExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleThreadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
