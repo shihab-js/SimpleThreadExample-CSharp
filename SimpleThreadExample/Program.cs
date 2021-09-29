76eaefb205e5bb9b626ba5a159edef2f463e1350
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-29 22:59:34 +0600

    program has been done...

--- SimpleThreadExample/Program.cs
+++ SimpleThreadExample/Program.cs
@@ -7,7 +7,16 @@ namespace SimpleThreadExample
     {
         static void Main(string[] args)
         {
-            
+            //create a current thread
+            Thread t = Thread.CurrentThread;
+
+            //set a name of t thread
+            t.Name = "Main Thread";
+
+            //print the name of thread
+            Console.WriteLine(t.Name);
+
+
         }
     }
 }
